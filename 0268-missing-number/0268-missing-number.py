695300b1338f2d719b73dc0c2d0e86d2515b025b
Alisha fatima <alishafatima6768@gmail.com>
2025-07-22 16:28:11 -0700

    Time: 0 ms (100%), Space: 18.7 MB (72.08%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -4,15 +4,3 @@ class Solution:
         exp_sum = n*(n+1)//2
         act_sum = sum(nums)
         return exp_sum - act_sum
-
-        # temp = n
-        # for i in range(n) :
-        #    temp ^= i
-        #    temp ^= nums[i]
-        # return temp
-
-        # nums.sort()
-        # for i in range(n) :
-        #    if nums[i] != i :
-        #        return i
-        # return i+1
