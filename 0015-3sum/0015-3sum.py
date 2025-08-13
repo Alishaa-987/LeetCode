9b8bb27ac26ca17350fec1b0f0378ed12c7df65b
Alisha fatima <alishafatima6768@gmail.com>
2025-08-13 10:41:44 +0500

    Time: 583 ms (55.25%), Space: 20.7 MB (36.08%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,7 +1,7 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        res = []
-        nums.sort()
+        res = []  # answer array
+        nums.sort() # sort the orignal array when you use two pointer approch
 
         for i in range(len(nums)):
             if i > 0 and nums[i] == nums[i-1]:
@@ -12,16 +12,19 @@ class Solution:
 
             while j < k:
                 total = nums[i] + nums[j] + nums[k]
-
-                if total > 0:
-                    k -= 1
-                elif total < 0:
-                    j += 1
-                else:
+                if total == 0:
                     res.append([nums[i], nums[j], nums[k]])
-                    j += 1
-
+                    j+=1
+                    k-=1
                     while nums[j] == nums[j-1] and j < k:
                         j += 1
-        
+                    while nums[k] == nums[k+1] and j< k:
+                        k-=1
+
+                elif total < 0:
+                    j+= 1
+                else :
+                    k-= 1
+           
+
         return res
