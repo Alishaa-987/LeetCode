eba2b08794f83d102793075a544b4297078a3472
Alisha fatima <alishafatima6768@gmail.com>
2025-09-08 12:00:13 +0500

    Time: 0 ms (100%), Space: 17.9 MB (63.85%) - LeetHub

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.py
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.py
@@ -1,21 +1,29 @@
 class MyQueue:
+
     def __init__(self):
-        self.s1 = []
-        self.s2 = []
+        self.inStack = []   # push karne ke liye
+        self.outStack = []  # pop/peek ke liye
 
-    def push(self, x):
-        while self.s1:
-            self.s2.append(self.s1.pop())
-        self.s1.append(x)
-        while self.s2:
-            self.s1.append(self.s2.pop())
+    def push(self, x: int) -> None:
+        # hamesha naya element inStack me daal do
+        self.inStack.append(x)
 
-    def pop(self):
-        return self.s1.pop()
+    def pop(self) -> int:
+        # agar outStack khali hai to inStack se sab transfer karo
+        if not self.outStack:
+            while self.inStack:
+                self.outStack.append(self.inStack.pop())
+        # ab outStack se pop karo
+        return self.outStack.pop()
 
-    def peek(self):
-        return self.s1[-1]
+    def peek(self) -> int:
+        # agar outStack khali hai to inStack se sab transfer karo
+        if not self.outStack:
+            while self.inStack:
+                self.outStack.append(self.inStack.pop())
+        # ab outStack ka top element return karo
+        return self.outStack[-1]
 
-    def empty(self):
-        return not self.s1
-
+    def empty(self) -> bool:
+        # dono stacks agar khali hain to queue bhi khali hai
+        return not self.inStack and not self.outStack
