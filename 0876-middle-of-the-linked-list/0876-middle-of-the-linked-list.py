1bd8bdaa0ee1057ee1d3d6fe1fb10129e0363289
Alisha fatima <alishafatima6768@gmail.com>
2025-10-22 12:17:14 +0500

    Time: 0 ms (100%), Space: 17.7 MB (49.06%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.py
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.py
@@ -3,6 +3,7 @@ class Solution:
         if not head or not head.next:
             return head
         slow, fast = head, head
+        
         while fast and fast.next:
             slow = slow.next
             fast = fast.next.next
