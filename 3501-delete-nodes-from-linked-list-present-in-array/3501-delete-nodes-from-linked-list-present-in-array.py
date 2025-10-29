4f340fd194daa2a87d56148c64c06f8712c97a3f
Alisha fatima <alishafatima6768@gmail.com>
2025-10-30 04:48:18 +0500

    Time: 72 ms (19.98%), Space: 58.5 MB (62.93%) - LeetHub

---
+++ 3501-delete-nodes-from-linked-list-present-in-array/3501-delete-nodes-from-linked-list-present-in-array.py
@@ -0,0 +1,19 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def modifiedList(self, nums: list[int], head: ListNode) -> ListNode:
+        to_delete = set(nums)
+        dummy = ListNode(0)
+        dummy.next = head
+        current = dummy
+
+        while current.next:
+            if current.next.val in to_delete:
+                current.next = current.next.next  # delete node
+            else:
+                current = current.next  # move ahead
+
+        return dummy.next
