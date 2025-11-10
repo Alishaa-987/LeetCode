0e824154cd12660b3b8246ab83dce3622cfb0e9c
Alisha fatima <alishafatima6768@gmail.com>
2025-11-10 09:28:36 +0500

    Time: 4 ms (15.11%), Space: 18 MB (17.47%) - LeetHub

--- 0202-happy-number/0202-happy-number.py
+++ 0202-happy-number/0202-happy-number.py
@@ -1,6 +1,5 @@
 class Solution:
     
-    # This function extracts the digits, squares them and return them back
     def fun(self, n):
         s = 0
         while n > 0:
@@ -11,11 +10,6 @@ class Solution:
         return s
         
     def isHappy(self, n: int) -> bool:
-        
-        # Arbitrarily to prevent the program from entering endless loop,
-        # I used a counter. The counter will count till 10 steps.
-        # If the program is still not able to produce 1 after 10 steps
-        # it ends. 
         count = 0
         while count < 10:
             n = self.fun(n)
