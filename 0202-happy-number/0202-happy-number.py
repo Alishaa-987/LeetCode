c4936aa8e4a8553285efe28298ec1ef79e6596cc
Alisha fatima <alishafatima6768@gmail.com>
2025-11-13 09:05:53 +0500

    Time: 0 ms (100%), Space: 18 MB (17.65%) - LeetHub

--- 0202-happy-number/0202-happy-number.py
+++ 0202-happy-number/0202-happy-number.py
@@ -1,23 +1,22 @@
 class Solution:
-    
-    def fun(self, n):
-        s = 0
-        while n > 0:
-            d = n % 10
-            s = s + d ** 2
-            n = n // 10
-        
-        return s
-        
     def isHappy(self, n: int) -> bool:
-        count = 0
-        while count < 10:
-            n = self.fun(n)
-            if n == 1:
-                return True
+    
+        def fun(n):
+            sum = 0
+            while n > 0:
+                d = n % 10
+                sum += d ** 2
+                n = n // 10
+            return sum 
+     
+        
+        slow = n
+        fast = fun(n)
+      
+        while fast != 1 and slow != fast:
+            slow = fun(slow)
+            fast = fun(fun(fast))
 
-            count += 1
-            
-        return False
+        return fast == 1
             
         
